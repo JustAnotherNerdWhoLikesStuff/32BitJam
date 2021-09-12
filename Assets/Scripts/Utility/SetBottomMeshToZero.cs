3ffd2b356c3c31e58832665f51fc291ff5ec64f1
Andrew Janssen <ajanssen@crimson.ua.edu>
2021-09-12 12:15:05 -0500

    #6: Added rudimentary player movement.  Also switched the axes of everything so the 2D plane is XY, and the vertical is Z.

--- Assets/Scripts/Utility/SetBottomMeshToZero.cs
+++ Assets/Scripts/Utility/SetBottomMeshToZero.cs
@@ -7,12 +7,12 @@ public class SetBottomMeshToZero : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(transform.position.x, transform.localScale.y / 2.0f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.localScale.y, transform.position.z / 2.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"Current y position is {transform.position.y}");
+
     }
 }
